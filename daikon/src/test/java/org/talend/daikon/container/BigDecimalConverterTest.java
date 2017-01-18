e4ce65dc155eae156ad5e9366f89cee678693492
pbailly <pbailly@talend.com>
2017-01-18 15:16:41 +0100

    feat(TCOMP-283): Fix after codacy's review

--- daikon/src/test/java/org/talend/daikon/container/BigDecimalConverterTest.java
+++ daikon/src/test/java/org/talend/daikon/container/BigDecimalConverterTest.java
@@ -13,120 +13,120 @@ import org.junit.Test;
  */
 public class BigDecimalConverterTest {
 
-    String input_String = "3"; //$NON-NLS-1$
+    private String input_String = "3"; //$NON-NLS-1$
 
-    String input_String_double = "3.0"; //$NON-NLS-1$
+    private String input_String_double = "3.0"; //$NON-NLS-1$
 
-    String input_String_empty = ""; //$NON-NLS-1$
+    private String input_String_empty = ""; //$NON-NLS-1$
 
-    String input_String_default = "0"; //$NON-NLS-1$
+    private String input_String_default = "0"; //$NON-NLS-1$
 
-    String input_String_default_double = "0.0"; //$NON-NLS-1$
+    private String input_String_default_double = "0.0"; //$NON-NLS-1$
 
-    String input_String_default_char = "a"; //$NON-NLS-1$
+    private String input_String_default_char = "a"; //$NON-NLS-1$
 
-    String input_String_default_boolean = "true"; //$NON-NLS-1$
+    private String input_String_default_boolean = "true"; //$NON-NLS-1$
 
-    String input_String_default_boolean_number = "1"; //$NON-NLS-1$
+    private String input_String_default_boolean_number = "1"; //$NON-NLS-1$
 
-    String input_String_null = null;
+    private String input_String_null = null;
 
-    java.nio.ByteBuffer input_ByteBuffer_null = null;
+    private java.nio.ByteBuffer input_ByteBuffer_null = null;
 
-    byte input_byte = 3;
+    private byte input_byte = 3;
 
-    byte input_byte_default = 0;
+    private byte input_byte_default = 0;
 
-    Byte input_Byte = 3;
+    private Byte input_Byte = 3;
 
-    Byte input_Byte_null = null;
+    private Byte input_Byte_null = null;
 
-    Byte input_Byte_default = 0;
+    private Byte input_Byte_default = 0;
 
-    boolean input_boolean = true;
+    private boolean input_boolean = true;
 
-    boolean input_boolean_default = false;
+    private boolean input_boolean_default = false;
 
-    Boolean input_Boolean = true;
+    private Boolean input_Boolean = true;
 
-    Boolean input_Boolean_default = false;
+    private Boolean input_Boolean_default = false;
 
-    Boolean input_Boolean_null = null;
+    private Boolean input_Boolean_null = null;
 
-    double input_double = 3.0d;
+    private double input_double = 3.0d;
 
-    double input_double_default = 0d;
+    private double input_double_default = 0d;
 
-    Double input_Double = 3.0D;
+    private Double input_Double = 3.0D;
 
-    Double input_Double_null = null;
+    private Double input_Double_null = null;
 
-    Double input_Double_default = 0D;
+    private Double input_Double_default = 0D;
 
-    float input_float = 3.0f;
+    private float input_float = 3.0f;
 
-    float input_float_default = 0f;
+    private float input_float_default = 0f;
 
-    Float input_Float = 3.0F;
+    private Float input_Float = 3.0F;
 
-    Float input_Float_null = null;
+    private Float input_Float_null = null;
 
-    Float input_Float_default = 0F;
+    private Float input_Float_default = 0F;
 
-    BigDecimal input_BigDecimal = new BigDecimal(3);
+    private BigDecimal input_BigDecimal = new BigDecimal(3);
 
-    BigDecimal output_BigDecimal_withScale = new BigDecimal(3).setScale(5);
+    private BigDecimal output_BigDecimal_withScale = new BigDecimal(3).setScale(5);
 
     // weird error case
-    BigDecimal input_BigDecimal_double = new BigDecimal(((Double) 3.000D).toString());
+    private BigDecimal input_BigDecimal_double = new BigDecimal(((Double) 3.000D).toString());
 
-    BigDecimal input_BigDecimal_null = null;
+    private BigDecimal input_BigDecimal_null = null;
 
-    BigDecimal input_BigDecimal_default = new BigDecimal(0);
+    private BigDecimal input_BigDecimal_default = new BigDecimal(0);
 
-    BigDecimal input_BigDecimal_defaultdouble = BigDecimal.valueOf(0.0d);
+    private BigDecimal input_BigDecimal_defaultdouble = BigDecimal.valueOf(0.0d);
 
-    int input_int = 3;
+    private int input_int = 3;
 
-    int input_int_default = 0;
+    private int input_int_default = 0;
 
-    Integer input_Integer = 3;
+    private Integer input_Integer = 3;
 
-    Integer input_Integer_null = null;
+    private Integer input_Integer_null = null;
 
-    Integer input_Integer_default = 0;
+    private Integer input_Integer_default = 0;
 
-    long input_long = 3l;
+    private long input_long = 3l;
 
-    long input_long_default = 0l;
+    private long input_long_default = 0l;
 
-    Long input_Long = 3L;
+    private Long input_Long = 3L;
 
-    Long input_Long_null = null;
+    private Long input_Long_null = null;
 
-    Long input_Long_default = 0L;
+    private Long input_Long_default = 0L;
 
-    short input_short = 3;
+    private short input_short = 3;
 
-    short input_short_default = 0;
+    private short input_short_default = 0;
 
-    Short input_Short = 3;
+    private Short input_Short = 3;
 
-    Short input_Short_null = null;
+    private Short input_Short_null = null;
 
-    Short input_Short_default = 0;
+    private Short input_Short_default = 0;
 
-    char input_char = 'a';
+    private char input_char = 'a';
 
-    char input_char_default = ' ';
+    private char input_char_default = ' ';
 
-    Character input_Char = 'a';
+    private Character input_Char = 'a';
 
-    Character input_Char_null = null;
+    private Character input_Char_null = null;
 
-    Character input_Char_default = ' ';
+    private Character input_Char_default = ' ';
 
-    String object_test = "a"; //$NON-NLS-1$
+    private String object_test = "a"; //$NON-NLS-1$
 
     @Test
     public void test_asBigDecimal_scale() {
